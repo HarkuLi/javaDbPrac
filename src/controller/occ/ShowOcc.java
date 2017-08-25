3fed1b1f26c238118034855e6c8d0bef934ca1b0
HarkuLi <harku85697708@hotmail.com>
2017-08-25 18:39:00 +0800

    new: servlet for getting occupation page, rename: servlet for showing jsp page of occupation

--- src/controller/occ/ShowOcc.java
+++ src/controller/occ/ShowOcc.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class OccListPage extends HttpServlet {
+public class ShowOcc extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse res)
