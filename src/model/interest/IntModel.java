5eff98086b9e1c0ab1b0a2ea4f5ebe40e7cb679a
HarkuLi <harku85697708@hotmail.com>
2017-08-28 18:00:42 +0800

    model of interest feat.

--- src/model/interest/IntModel.java
+++ src/model/interest/IntModel.java
@@ -1,5 +1,27 @@
 package model.interest;
 
-public class IntModel {
+import java.util.HashMap;
 
+public class IntModel {
+	private String id;
+	private String name;
+	private boolean state;
+	
+	public IntModel(HashMap<String, Object> data) {
+		id = (String) data.get("id");
+		name = (String) data.get("name");
+		state = (boolean) data.get("state");
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public boolean getState() {
+		return state;
+	}
 }
