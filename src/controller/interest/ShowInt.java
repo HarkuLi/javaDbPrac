b8e086bd0454bfcf4d15e1e6f4eaba48c254f4e0
HarkuLi <harku85697708@hotmail.com>
2017-08-28 17:47:19 +0800

    servlet for showing interest page

--- src/controller/interest/ShowInt.java
+++ src/controller/interest/ShowInt.java
@@ -1,5 +1,20 @@
 package controller.interest;
 
-public class ShowInt {
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ShowInt extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+		throws ServletException, IOException {
+		
+		RequestDispatcher rd = req.getRequestDispatcher("/interest.jsp");
+		rd.forward(req, res);
+	}
 }
