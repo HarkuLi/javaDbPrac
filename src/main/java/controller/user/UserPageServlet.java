ba34155730851aaac149119acf3167ca9b1ecfc3
HarkuLi <harku85697708@hotmail.com>
2017-09-29 18:12:45 +0800

    merge controllers of user feat. into two class

--- src/main/java/controller/user/UserPageServlet.java
+++ src/main/java/controller/user/UserPageServlet.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/user")
-public class ShowUsers {
+public class UserPageServlet {
 	@RequestMapping(value = "/page", method = RequestMethod.GET)
-	public String get(ModelMap model) {
-		System.out.println("spring controller");
+	public String ShowPage(ModelMap model) {
+		System.out.println("user page");
 		return "user";
 	}
 }
