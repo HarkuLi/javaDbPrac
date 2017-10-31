f3586bb8c5686088879afefac862d4e3af2dae1e
HarkuLi <harku85697708@hotmail.com>
2017-10-31 17:39:32 +0800

    add handler for missing required parameters

--- src/main/java/com/harku/aspect/GlobalControllerAdvice.java
+++ src/main/java/com/harku/aspect/GlobalControllerAdvice.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,7 +22,7 @@ public class GlobalControllerAdvice {
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	@ExceptionHandler(value = Exception.class)
 	@ResponseBody
-	public Object exceptionHandler(HttpServletRequest req, HttpServletResponse res, Exception e) {
+	public Object rootExceptionHandler(HttpServletRequest req, HttpServletResponse res, Exception e) {
 		String className = e.getStackTrace()[0].getClassName();
 		String methodName = e.getStackTrace()[0].getMethodName();
 		String sourceName = className + "." + methodName;
@@ -29,6 +30,22 @@ public class GlobalControllerAdvice {
 		
 		Map<String, Object> msg = new HashMap<String, Object>();
 		msg.put("errMsg", "there are some errors in the server.");
+		return msg;
+	}
+	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(value = MissingServletRequestParameterException.class)
+	@ResponseBody
+	public Object missingParameterHandler(HttpServletRequest req, HttpServletResponse res, Exception e) {
+		String className = e.getStackTrace()[0].getClassName();
+		String methodName = e.getStackTrace()[0].getMethodName();
+		String sourceName = className + "." + methodName;
+		String errMsg = e.toString().split(": ")[1];
+		
+		log.info("[{}] Call API without required parameters: {}", sourceName, errMsg);
+		
+		Map<String, Object> msg = new HashMap<String, Object>();
+		msg.put("errMsg", errMsg);
 		return msg;
 	}
 }
