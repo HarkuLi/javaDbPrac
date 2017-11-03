b4eabc27ddff7a0fa99afd4b9573507b7c0adf98
HarkuLi <harku85697708@hotmail.com>
2017-11-03 18:32:06 +0800

    modify corresponding test case after modifying the validator

--- src/test/java/com/harku/test/validator/TestAccountValidator.java
+++ src/test/java/com/harku/test/validator/TestAccountValidator.java
@@ -1,21 +1,15 @@
 package com.harku.test.validator;
 
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mindrot.jbcrypt.BCrypt;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.Errors;
 
 import com.harku.model.user.UsersModel;
-import com.harku.service.user.UserAccService;
 import com.harku.test.util.RandomData;
 import com.harku.validator.user.AccountValidator;
 
@@ -23,17 +17,13 @@ import com.harku.validator.user.AccountValidator;
 public class TestAccountValidator {
 	private final String accountField = "account";
 	private final String passwordField = "password";
-	private final String errorMsgName = "account.noMatch";
+	private final String accountErrorMsgName = "account.notFilled";
+	private final String passwordErrorMsgName = "password.notFilled";
 	
 	@Mock
 	private Errors e;
 	
-	@Mock
-	private UserAccService UAS;
-	
-	@Autowired
-	@InjectMocks
-	private AccountValidator accountValidator;
+	private AccountValidator accountValidator = new AccountValidator();
 	
 	@Test
 	public void validAccount() {
@@ -41,83 +31,68 @@ public class TestAccountValidator {
 		UsersModel inputAccount = RandomData.genUser();
 		String plainPassword = RandomData.genStr(8, 20);
 		inputAccount.setPassword(plainPassword);
-		String hashedPassword = BCrypt.hashpw(plainPassword, BCrypt.gensalt(RandomData.workload));
-		
-		//generate account read from db
-		UsersModel DBAccount = new UsersModel();
-		DBAccount.setAccount(inputAccount.getAccount());
-		DBAccount.setPassword(hashedPassword);
-		DBAccount.setState(true);
-		
-		//set Stub
-		when(UAS.getAcc(inputAccount.getAccount())).thenReturn(DBAccount);
-		
+				
 		//call function
 		accountValidator.validate(inputAccount, e);
 		
 		//verify
-		verify(e, never()).rejectValue(accountField, errorMsgName);
+		verify(e, never()).rejectValue(accountField, accountErrorMsgName);
+		verify(e, never()).rejectValue(passwordField, passwordErrorMsgName);
 	}
 	
 	@Test
-	public void notExistingAccount() {
-		UsersModel inputAccount = RandomData.genUser();
-		String plainPassword = RandomData.genStr(8, 20);
-		inputAccount.setPassword(plainPassword);
-		
-		//set Stub
-		when(UAS.getAcc(inputAccount.getAccount())).thenReturn(null);
-		
-		//call function
-		accountValidator.validate(inputAccount, e);
-		
-		//verify
-		verify(e, times(1)).rejectValue(accountField, errorMsgName);
-	}
-	
-	@Test
-	public void disabledAccount() {
+	public void nullAccount() {
 		//generate the account that user inputs
 		UsersModel inputAccount = RandomData.genUser();
 		String plainPassword = RandomData.genStr(8, 20);
+		inputAccount.setAccount(null);
 		inputAccount.setPassword(plainPassword);
-		
-		//generate account read from db
-		UsersModel DBAccount = new UsersModel();
-		DBAccount.setAccount(inputAccount.getAccount());
-		DBAccount.setState(false);
-		
-		//set Stub
-		when(UAS.getAcc(inputAccount.getAccount())).thenReturn(DBAccount);
-		
+				
 		//call function
 		accountValidator.validate(inputAccount, e);
 		
 		//verify
-		verify(e, times(1)).rejectValue(accountField, errorMsgName);
+		verify(e).rejectValue(accountField, accountErrorMsgName);
 	}
 	
 	@Test
-	public void onlyWrongPassword() {
+	public void zeroLengthAccount() {
 		//generate the account that user inputs
 		UsersModel inputAccount = RandomData.genUser();
 		String plainPassword = RandomData.genStr(8, 20);
+		inputAccount.setAccount("");
 		inputAccount.setPassword(plainPassword);
-		
-		//generate account read from db
-		UsersModel DBAccount = new UsersModel();
-		DBAccount.setAccount(inputAccount.getAccount());
-		String hashedPassword = BCrypt.hashpw(plainPassword + "123", BCrypt.gensalt(RandomData.workload));
-		DBAccount.setPassword(hashedPassword);
-		DBAccount.setState(true);
-		
-		//set Stub
-		when(UAS.getAcc(inputAccount.getAccount())).thenReturn(DBAccount);
-		
+				
 		//call function
 		accountValidator.validate(inputAccount, e);
 		
 		//verify
-		verify(e, times(1)).rejectValue(passwordField, errorMsgName);
+		verify(e).rejectValue(accountField, accountErrorMsgName);
+	}
+	
+	@Test
+	public void nullPassword() {
+		//generate the account that user inputs
+		UsersModel inputAccount = RandomData.genUser();
+		inputAccount.setPassword(null);
+				
+		//call function
+		accountValidator.validate(inputAccount, e);
+		
+		//verify
+		verify(e).rejectValue(passwordField, passwordErrorMsgName);
+	}
+	
+	@Test
+	public void zeroLengthPassword() {
+		//generate the account that user inputs
+		UsersModel inputAccount = RandomData.genUser();
+		inputAccount.setPassword("");
+				
+		//call function
+		accountValidator.validate(inputAccount, e);
+		
+		//verify
+		verify(e).rejectValue(passwordField, passwordErrorMsgName);
 	}
 }
