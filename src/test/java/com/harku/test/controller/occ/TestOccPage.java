ad07711d5e766d4cf5efd6e9f235ce0dece8be12
HarkuLi <harku85697708@hotmail.com>
2017-11-06 13:49:19 +0800

    test case of /occ/page

---
+++ src/test/java/com/harku/test/controller/occ/TestOccPage.java
@@ -0,0 +1,38 @@
+package com.harku.test.controller.occ;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.harku.config.WebConfig;
+import com.harku.controller.occ.OccPageController;
+import com.harku.test.config.ViewResolverTest;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = WebConfig.class)
+public class TestOccPage {
+	private MockMvc mockMvc;
+	
+	@Before
+	public void init() {
+		mockMvc = MockMvcBuilders
+				.standaloneSetup(new OccPageController())
+				.setViewResolvers(ViewResolverTest.genResolver())
+				.build();
+	}
+	
+	@Test
+	public void showPage() throws Exception {
+		mockMvc.perform(get("/occ/page"))
+				.andExpect(status().isOk());
+	}
+}
