9c588d99c7d2fad4e388d25128529c601fba505e
HarkuLi <harku85697708@hotmail.com>
2017-11-10 11:48:54 +0800

    test cases for new APIs

---
+++ src/test/java/com/harku/test/controller/user/TestUserEditForm.java
@@ -0,0 +1,37 @@
+package com.harku.test.controller.user;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.harku.config.WebConfig;
+import com.harku.controller.user.UserPageController;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = WebConfig.class)
+public class TestUserEditForm {
+	private MockMvc mockMvc;
+	
+	@Before
+	public void init() {
+		mockMvc = MockMvcBuilders
+				.standaloneSetup(new UserPageController())
+				.setViewResolvers(new WebConfig().viewResolver())
+				.build();
+	}
+	
+	@Test
+	public void showPage() throws Exception {
+		mockMvc.perform(get("/user/edit_form"))
+				.andExpect(status().isOk());
+	}
+}
